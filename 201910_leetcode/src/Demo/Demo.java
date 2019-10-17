2ac3d8b11824a4499af6d43830c08d57ba27d83d
DD <32081227+YuanlingGl@users.noreply.github.com>
2019-10-17 21:31:18 +0800

    20191017

---
+++ 201910_leetcode/src/Demo/Demo.java
@@ -0,0 +1,15 @@
+package Demo;
+
+/**
+ * @program: 201910_leetcode
+ * @description:
+ * @author: dd
+ * @created: 2019/10/17 16:32
+ */
+public class Demo {
+    public static void main(String[] args) {
+//        String a = "Hello World !";
+//        String b =a.substring ( 6, 12);
+//        System.out.println(b);
+    }
+}
