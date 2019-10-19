00ffb7c990688683e72327c0550b65107e57360a
DD <32081227+YuanlingGl@users.noreply.github.com>
2019-10-19 21:56:06 +0800

    20191019

---
+++ 201910_leetcode/src/product_of_array_except_self/Demo.java
@@ -0,0 +1,26 @@
+package product_of_array_except_self;
+
+/**
+ * @program: 201910_leetcode
+ * @description:
+ * @author: dd
+ * @created: 2019/10/19 21:36
+ */
+public class Demo {
+}
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+        int k = 1;
+        for(int i = 0; i < res.length; i++){
+            res[i] = k;
+            k = k * nums[i]; // 此时数组存储的是除去当前元素左边的元素乘积
+        }
+        k = 1;
+        for(int i = res.length - 1; i >= 0; i--){
+            res[i] *= k; // k为该数右边的乘积。
+            k *= nums[i]; // 此时数组等于左边的 * 该数右边的。
+        }
+        return res;
+    }
+}
