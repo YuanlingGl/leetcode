6b9d398a99b9348ffcdc12057f24051c67b8f719
DD <32081227+YuanlingGl@users.noreply.github.com>
2019-10-25 21:48:08 +0800

    20191025

---
+++ 201910_leetcode/src/Demo/Demo2.java
@@ -0,0 +1,20 @@
+package Demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @program: 201910_leetcode
+ * @description:
+ * @author: dd
+ * @created: 2019/10/25 16:22
+ */
+public class Demo2 {
+    public static void main(String[] args) {
+        List list = new ArrayList();
+        list.add(0,0);
+        list.add(1,1);
+        list.add(2,2);
+        System.out.println(list.get(0)+"1211");
+    }
+}
