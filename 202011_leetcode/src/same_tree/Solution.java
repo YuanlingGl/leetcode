c0811f12fbd8ae45a676d1b64677e59b433a3fd7
yuanling <haolingyuandd@gmail.com>
2020-11-04 17:22:10 +0800

    feat(100. 相同的树): 二叉树 递归 深度优先
    https://leetcode-cn.com/problems/same-tree/

---
+++ 202011_leetcode/src/same_tree/Solution.java
@@ -0,0 +1,31 @@
+package same_tree;
+
+/**
+ * @author dd
+ * @version 1.0
+ * @date 2020/11/4 17:16
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p==null&&q==null){
+            return true;
+        }else if (p==null||q==null){
+            return false;
+        }else if (p.val!=q.val){
+            return false;
+        }
+        return isSameTree(p.left, q.left)&&isSameTree(p.right,q.right);
+    }
+}
