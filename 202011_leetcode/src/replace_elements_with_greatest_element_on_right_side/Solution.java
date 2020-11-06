0db99b0891a5f7a73f4121d2c602ba376209c2f3
yuanling <haolingyuandd@gmail.com>
2020-11-06 10:33:02 +0800

    feat(1299. 将每个元素替换为右侧最大元素): 数组遍历
    https://leetcode-cn.com/problems/replace-elements-with-greatest-element-on-right-side/

---
+++ 202011_leetcode/src/replace_elements_with_greatest_element_on_right_side/Solution.java
@@ -0,0 +1,20 @@
+package replace_elements_with_greatest_element_on_right_side;
+
+/**
+ * @author dd
+ * @version 1.0
+ * @date 2020/11/6
+ */
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        int maxRightNum =-1;
+        for (int i= arr.length-1;i>=0;i--){
+            int temp = arr[i];
+            arr[i] = maxRightNum;
+            if (temp > maxRightNum) {
+                maxRightNum = temp;
+            }
+        }
+        return arr;
+    }
+}
