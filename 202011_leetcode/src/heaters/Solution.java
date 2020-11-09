3a1943596ab9c2b821299f8a1706f8c3bd2c76f5
yuanling <haolingyuandd@gmail.com>
2020-11-09 16:53:00 +0800

    feat(475. 供暖器): 双指针 贪心算法?
    https://leetcode-cn.com/problems/heaters/

---
+++ 202011_leetcode/src/heaters/Solution.java
@@ -0,0 +1,25 @@
+package heaters;
+
+import java.util.Arrays;
+import java.util.Map;
+
+/**
+ * @author dd
+ * @version 1.0
+ * @date 2020/11/9
+ */
+class Solution {
+    public int findRadius(int[] houses, int[] heaters) {
+        Arrays.sort(houses);
+        Arrays.sort(heaters);
+        int i = 0;
+        int re = 0;
+        for (int house:houses){
+            while (i<heaters.length-1 && Math.abs(heaters[i]-house)>=heaters[i+1]-house){
+                i++;
+            }
+            re = Math.max(re,Math.abs(heaters[i]-house));
+        }
+            return re;
+    }
+}
