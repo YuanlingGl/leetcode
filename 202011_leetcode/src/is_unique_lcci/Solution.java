61d3b2692424f99633a7bb7d1d06d3b2ad41cd44
yuanling <haolingyuandd@gmail.com>
2020-11-16 15:19:09 +0800

    feat(面试题 01.01. 判定字符是否唯一): Set
    https://leetcode-cn.com/problems/is-unique-lcci/
    
    也可以维护一个数组，char[26]
    这样来表示26个字母
    
    写程序需要先确定需求

--- 202011_leetcode/src/is_unique_lcci/Solution.java
+++ 202011_leetcode/src/is_unique_lcci/Solution.java
@@ -12,13 +12,26 @@ class Solution {
     public boolean isUnique(String astr) {
         char[] a = astr.toCharArray();
         Set<Character> set = new HashSet();
-        for (char b: a
-             ) {
-            if (set.contains(b)){
+        for (char b : a
+        ) {
+            if (set.contains(b)) {
                 return false;
-            }
-            else {
+            } else {
                 set.add(b);
+            }
+        }
+        return true;
+    }
+}
+
+class Solution1 {
+    public boolean isUnique(String astr) {
+        char[] hash = new char[26];
+        for (char c : astr.toCharArray()
+        ) {
+            hash[c - 'a']++;
+            if (hash[c - 'a'] > 1) {
+                return false;
             }
         }
         return true;
