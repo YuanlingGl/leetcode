87c5308ebe125c267fda7395c01d90d306e67102
yuanling <haolingyuandd@gmail.com>
2020-11-17 14:45:58 +0800

    build(面试题 01.03. URL化): 字符数组
    https://leetcode-cn.com/problems/string-to-url-lcci/

--- 202011_leetcode/src/string_to_url_lcci/Solution.java
+++ 202011_leetcode/src/string_to_url_lcci/Solution.java
@@ -8,30 +8,35 @@ package string_to_url_lcci;
 class Solution {
     public String replaceSpaces(String S, int length) {
         char[] c1 = S.toCharArray();
-        char[] c2 = new char[length];
-        for (int i = 0, j = 0; i < S.length(); i++) {
+
+        int K = 0;
+        for (int i = 0; i < length; i++) {
+            if (c1[i] == ' ') {
+                K++;
+            }
+        }
+        char[] c2 = new char[K * 2 + length];
+        for (int i = 0, j = 0; i < length; i++) {
             if ((c1[i]) == ' ') {
-                c2[j]='%';
-                j++;
-                c2[j]='2';
-                j++;
-                c2[j]='0';
-            }else {
-                c2[j]=c1[i];
-                j++;
+                c2[j++] = '%';
+                c2[j++] = '2';
+                c2[j++] = '0';
+            } else {
+                c2[j++] = c1[i];
             }
         }
         StringBuilder sb = new StringBuilder();
-        for (char c: c2
-             ) {
+        for (char c : c2
+        ) {
             sb.append(c);
         }
         return sb.toString();
     }
 }
-class Demo{
+
+class Demo {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.replaceSpaces("Mr John Smith    ",13));
+        System.out.println(solution.replaceSpaces("Mr John Smith    ", 13));
     }
 }
