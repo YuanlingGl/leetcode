4195f09201e845e8c8c9c3e81c556e820573e005
yuanling <haolingyuandd@gmail.com>
2020-11-18 15:10:34 +0800

    ci(134. 加油站): 一次遍历 贪心算法
    https://leetcode-cn.com/problems/gas-station/

---
+++ 202011_leetcode/src/gas_station/Solution.java
@@ -0,0 +1,23 @@
+package gas_station;
+
+/**
+ * @author dd
+ * @version 1.0
+ * @date 2020/11/18
+ */
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int len = gas.length;
+        int spare = 0;
+        int minSpare = Integer.MAX_VALUE;
+        int minIndex = 0;
+        for (int i = 0; i < len; i++) {
+            spare += gas[i] - cost[i];
+            if (spare < minSpare) {
+                minSpare = spare;
+                minIndex = i;
+            }
+        }
+        return spare < 0 ? -1 : (minIndex + 1) % len;
+    }
+}
