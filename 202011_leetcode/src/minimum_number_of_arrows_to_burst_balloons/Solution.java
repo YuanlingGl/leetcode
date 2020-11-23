c6f58415a4a139044134248bf304759f8ec76abb
yuanling <haolingyuandd@gmail.com>
2020-11-23 15:22:31 +0800

    feat(452. 用最少数量的箭引爆气球): 贪心算法 排序
    https://leetcode-cn.com/problems/minimum-number-of-arrows-to-burst-balloons/

---
+++ 202011_leetcode/src/minimum_number_of_arrows_to_burst_balloons/Solution.java
@@ -0,0 +1,26 @@
+package minimum_number_of_arrows_to_burst_balloons;
+
+import java.util.Arrays;
+
+/**
+ * @author dd
+ * @version 1.0
+ * @date 2020/11/23
+ */
+class Solution {
+    public int findMinArrowShots(int[][] points) {
+        if (points.length == 0) {
+            return 0;
+        }
+        Arrays.sort(points, (p1, p2) -> p1[1] < p2[1] ? -1 : 1);
+        int res = 1;
+        int pre = points[0][1];
+        for (int i = 1; i < points.length; i++) {
+            if (points[i][0] > pre) {
+                res++;
+                pre = points[i][1];
+            }
+        }
+        return res;
+    }
+}
