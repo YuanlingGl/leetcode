15665348dcf757f6b53866b570676f19824f90ba
yuanling <haolingyuandd@gmail.com>
2020-11-25 18:12:18 +0800

    feat(1370. 上升下降字符串):
    https://leetcode-cn.com/problems/increasing-decreasing-string/

---
+++ 202011_leetcode/src/increasing_decreasing_string/Solution.java
@@ -0,0 +1,32 @@
+package increasing_decreasing_string;
+
+/**
+ * @author dd
+ * @version 1.0
+ * @date 2020/11/25
+ */
+class Solution {
+    public String sortString(String s) {
+        int[] num = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            num[s.charAt(i) - 'a']++;
+        }
+
+        StringBuffer ret = new StringBuffer();
+        while (ret.length() < s.length()) {
+            for (int i = 0; i < 26; i++) {
+                if (num[i] > 0) {
+                    ret.append((char) (i + 'a'));
+                    num[i]--;
+                }
+            }
+            for (int i = 25; i >= 0; i--) {
+                if (num[i] > 0) {
+                    ret.append((char) (i + 'a'));
+                    num[i]--;
+                }
+            }
+        }
+        return ret.toString();
+    }
+}
