dc7565f1c806508894e5cc7e29a7bfdea30f9c05
yuanling <haolingyuandd@gmail.com>
2020-11-27 18:07:02 +0800

    feat(454. 四数相加 II): 分组、哈希
    https://leetcode-cn.com/problems/4sum-ii/

---
+++ 202011_leetcode/src/ii_4sum_ii/Solution.java
@@ -0,0 +1,29 @@
+package ii_4sum_ii;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author dd
+ * @version 1.0
+ * @date 2020/11/27
+ */
+class Solution {
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        Map<Integer, Integer> countAB = new HashMap<Integer, Integer>();
+        for (int u : A) {
+            for (int v : B) {
+                countAB.put(u + v, countAB.getOrDefault(u + v, 0) + 1);
+            }
+        }
+        int ans = 0;
+        for (int u : C) {
+            for (int v : D) {
+                if (countAB.containsKey(-u - v)) {
+                    ans += countAB.get(-u - v);
+                }
+            }
+        }
+        return ans;
+    }
+}
